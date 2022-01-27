c46b3b1103d77c02d2f0a95963b3eed1df8bae10
Thiago <thiagoautran@gmail.com>
2022-01-26 23:14:22 -0300

    .

---
+++ TAN.Core.6.0.Example.GraphQL/Controllers/v1/SkokkaController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+
+namespace TAN.Core._5._0.Skokka.Api.Controllers.v1
+{
+    [Produces("application/json")]
+    [ApiVersion("1")]
+    [ApiExplorerSettings(GroupName = "v1")]
+    [ApiController]
+    [Route("api/v{version:apiVersion}/skokka")]
+    public class SkokkaController : ControllerBase
+    {
+        public SkokkaController()
+        {
+        }
+
+        [HttpGet]
+        [MapToApiVersion("1")]
+        public async Task<ContentResult> GetEscortsHtmlAsync()
+        {
+            return new ContentResult 
+            {
+                ContentType = "text/html",
+                Content = string.Empty
+            };
+        }
+    }
+}
---
+++ TAN.Core.6.0.Example.GraphQL/Startup.cs
@@ -0,0 +1,45 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace TAN.Core._5._0.Skokka.Api
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddApiVersioning();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
