80e10c03d42551b0302e191f9093a80f932fe295
Thiago <thiagoautran@gmail.com>
2022-01-27 01:34:36 -0300

    .

--- TAN.Core.6.0.Example.ApplicationCore/Types/ProductType.cs
+++ TAN.Core.6.0.Example.ApplicationCore/Types/ProductType.cs
@@ -7,9 +7,11 @@ namespace TAN.Core._6._0.Example.ApplicationCore.Types
     {
         public ProductType()
         {
-            Field(product => product.Id).Description("Identificador unico do produto.");
-            Field(product => product.Name).Description("Nome do produto.");
-            Field(product => product.Description).Description("Descrição do produto.");
+            Name = "Produto";
+            Description = "Descrição do Produto";
+            Field(product => product.Id, nullable: false).Description("Identificador unico do produto.");
+            Field(product => product.Name, nullable: true).Description("Nome do produto.");
+            Field(product => product.Description, nullable: true).Description("Descrição do produto.");
         }
     }
 }
